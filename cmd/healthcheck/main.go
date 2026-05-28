30b196173e3c8064c23bf20752ed7784712f1c30
Luna Seemann <luna@wamellow.com>
2026-05-28 16:50:13 +0200

    fix: healthcheck client timeout and close response body

--- cmd/healthcheck/main.go
+++ cmd/healthcheck/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
-	resp, err := http.Get("http://localhost:8080/health")
+	client := &http.Client{Timeout: 4 * time.Second}
+	resp, err := client.Get("http://localhost:8080/health")
 	if err != nil || resp.StatusCode != http.StatusNoContent {
 		os.Exit(1)
 	}
+	defer resp.Body.Close()
 }
